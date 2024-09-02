1860cd9da2f9ae48f52f6e3f6683ba13ab878ac3
Hawayasin <yasinmanikhawa@mail.ugm.ac.id>
2024-09-02 11:02:25 +0700

    gatau nambahin apa yang penting ke repair

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { AppStoreModule } from 'src/store/AppStoreModule';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent],
+    LoadingComponent,
+    entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ...AppStoreModule,
+StoreDevtoolsModule.instrument({maxAge: 25})
+],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
